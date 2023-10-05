b584e79d67570dceb2557984aa311fb645bd0712
Shiwei9984 <126206359+Shiwei9984@users.noreply.github.com>
2023-10-05 15:33:41 -0400

    Update script.js

---
+++ Level 1 JS project/script.js
@@ -0,0 +1,21 @@
+const addButtonPenny = document.getElementById("addForPenny");
+const subtractButtonPenny = document.getElementById("subtractForPenny");
+const addButtonNickle = document.getElementById("addForNickle");
+const subtractButtonNickle = document.getElementById("subtractForNickle");
+const addButtonDime = document.getElementById("addForDime");
+const subtractButtonDime = document.getElementById("subtractForDime");
+const addButtonQuarter = document.getElementById("addForQuarter");
+const subtractButtonQuarter = document.getElementById("subtractForQuarter");
+//buttons
+let pennyNum = document.getElementById("penny#");
+let pennyVal = document.getElementById("pennyVal");
+//Num and Val
+let Penny = 0;
+let penny = 0;
+
+//temporary variable
+//functions
+function addPenny(){
+  Penny++;
+  penn
+}
